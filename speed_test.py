16061b677bda4c29d8a26440302ee7f9869ce752
svartse <killbillbeatrixx@gmail.com>
2023-07-02 02:39:44 +0300

    label tags

--- speed_test.py
+++ speed_test.py
@@ -2,11 +2,23 @@ import tkinter as tk
 import speedtest_cli as speedtest
 
 root = tk.Tk()
-label = tk.Label(root, text="Hello, tkinter!")
-label.pack()
-root.title("Internet Speed Test")
-root.geometry("360x600")
+root.title("SPEEDY")
+root.geometry("310x550")
 root.resizable(False, False)
-root.configure(bg="#1a212d")
+root.configure(bg="#1f1d2d")
+
+#icon
+image_icon = tk.PhotoImage(file='logo.png')
+root.iconphoto(False, image_icon)
+
+#images
+Top = tk.PhotoImage(file='indicators.png')
+label = tk.Label(root, image=Top, bg='#1f1d2d').pack() 
+
+Main = tk.PhotoImage(file='main.png')
+label = tk.Label(root, image=Main, bg='#1f1d2d').pack()
+
+Button = tk.PhotoImage(file='button.png')
+label = tk.Label(root, image=Button, bg='#1f1d2d').pack()
 
 root.mainloop()
