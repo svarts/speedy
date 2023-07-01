645135264fb5d09d04c6c4ddc7cd59b453b853e6
svartse <killbillbeatrixx@gmail.com>
2023-07-02 00:20:50 +0300

    first commit

---
+++ speed_test.py
@@ -0,0 +1,10 @@
+import thinker as tk 
+import speedtest 
+
+root = tk.Tk()
+root.title("Internet Speed Test")
+root.geometry("360x600")
+root.resizable(False, False)
+root.configure(bg="#1a212d")
+
+root.mainloop()
