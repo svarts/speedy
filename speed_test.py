8b4c01614b680d74e98a3fc8a6fff0c6120ae137
svartse <killbillbeatrixx@gmail.com>
2023-07-02 00:36:31 +0300

    tkinter imported

--- speed_test.py
+++ speed_test.py
@@ -1,7 +1,9 @@
-import thinker as tk 
+import tkinter as tk 
 import speedtest 
 
 root = tk.Tk()
+label = tk.Label(root, text="Hello, tkinter!")
+label.pack()
 root.title("Internet Speed Test")
 root.geometry("360x600")
 root.resizable(False, False)
